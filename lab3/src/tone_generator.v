85915109811a66ff0c081311b0f9fe5d9f52d26c
Kareem Ahmad <kareem.algalaly@berkely.edu>
2020-09-06 22:21:43 -0600

    Makefile & tcl scripts now support hw_server port overriding

---
+++ lab3/src/tone_generator.v
@@ -0,0 +1,11 @@
+`timescale 1ns/1ns
+module tone_generator (
+    input clk,
+    input output_enable,
+    input [23:0] tone_switch_period,
+    input volume,
+    output square_wave_out
+);
+    // TODO(you): Your code here. Remove the following lines once you add your implementation.
+    assign square_wave_out = 1'b0;
+endmodule
---
+++ lab3/src/z1top.v
@@ -0,0 +1,14 @@
+`timescale 1ns/1ns
+
+module z1top (
+    input CLK_125MHZ_FPGA,
+    input [3:0] BUTTONS,
+    input [1:0] SWITCHES,
+    output [5:0] LEDS,
+    output aud_pwm,
+    output aud_sd
+);
+    // TODO(you): Your code here. Remove the following lines once you add your implementation.
+    assign LEDS[5:0] = 6'b111111;
+    assign aud_sd = 0;
+endmodule
